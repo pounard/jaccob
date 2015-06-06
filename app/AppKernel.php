d59fbb7dbe17a9154db48995db7578ca28f1c6ec
Pierre Rineau <pierre.rineau@makina-corpus.com>
2015-06-06 14:28:08 +0200

    Primary integration with bootstrap 3

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new PommProject\PommBundle\PommBundle(),
+            new Jaccob\AppBundle\JaccobAppBundle(),
             new Jaccob\AccountBundle\JaccobAccountBundle(),
         );
 
