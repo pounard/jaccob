13b9f5e0f7f1f97f80adad9ff6f085579b348466
Pierre Rineau <pierre.rineau@processus.org>
2015-11-22 22:17:55 +0100

    Fixed nginx redis script, first working version, still a bit slow

--- src/Jaccob/MediaBundle/Resources/nginx/auth-redis.lua
+++ src/Jaccob/MediaBundle/Resources/nginx/auth-redis.lua
@@ -1,16 +1,48 @@
-# Fake infos from nginx
-local cookie = "9qubasgs56uj9oirioap7vieq5"
-local albumid = 28
+--
+-- Add to your nginx configuration:
+--
+--     location ~ ^/media/th/.* {
+--         access_by_lua_file /DOCROOT/src/Jaccob/MediaBundle/Resources/nginx/auth-redis.lua
+--     }
+--
+-- This requires that:
+--
+--   * the jaccob_media.security.external service is set to use the
+--     Jaccob\MediaBundle\Security\External\RedisSessionAclManager class.
+--
+--   * the jaccob_media.path_builder service is set to use the
+--     Jaccob\MediaBundle\Util\SimplePathBuilder
+--
 
-local driver = require "luasql.postgres"
-local env = driver.postgres()
-local conn = env:connect("jaccob", "jaccob", "jaccob")
+-- Fake infos from nginx for testing purpose, comment those two then uncomment
+-- the next two for the real environement
+-- local cookie = "vuldhtq1tc7802gvvpcjveb565"
+-- local path = "/media/th/w1440/2015/11/1/IMG_1176.JPG";
+local path = ngx.var.uri
+local cookie = ngx.var.cookie_PHPSESSID
 
-local cur = conn:execute(string.format("select 1 as win from external_session_acl where id_album = %d and id_session = '%s'", conn:escape(albumid), conn:escape(cookie)))
-
-local row = cur:fetch({}, "a")
-while row do
-    print(string.format("%s", row.win))
-    row = cur:fetch(row, "a")
+-- Derivate album from the URI
+-- God LUA seems not really helpful with strings
+local args = string.gmatch(path, "[^/]+")
+local index = 0
+local albumid
+for arg in args do
+    if index == 5 then
+        albumid = arg
+        break
+    end
+    index = index + 1
 end
 
+-- Connect to postgres and fetch album status for session
+local redis = require 'redis'
+-- @todo Variabilise this
+local client = redis.connect('127.0.0.1', 6379)
+client:select(7)
+local result = client:get(cookie .. ":" .. albumid)
+
+if result then
+    return
+else
+    ngx.exit(ngx.HTTP_FORBIDDEN)
+end
